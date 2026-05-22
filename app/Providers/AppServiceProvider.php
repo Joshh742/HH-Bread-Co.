3673ee8e7ad27c7696e69daa9ea6fe3b19af024f
Joshh742 <revildobusdan@gmail.com>
2026-05-22 17:38:47 +0800

    Memunculkan Logo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Ini yang baru ditambahkan
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Memaksa Laravel memuat aset (gambar/css) menggunakan HTTPS di server Railway
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
